573e278b11b40df0bac3957d784c81ffe3f7d0e6
Ankit Bhardwaj <iamankitbhardwaj@gmail.com>
2020-05-06 11:36:05 +0530

    - Mostly final app - onClick implemented on Contact items Features to be added : - Add more info in about me tab - Reduce scrolling senstivity of viewpager
    Signed-off-by: Ankit Bhardwaj <iamankitbhardwaj@gmail.com>

--- app/src/main/java/com/bhardwaj/portfolioapp/ContactAdapter.kt
+++ app/src/main/java/com/bhardwaj/portfolioapp/ContactAdapter.kt
@@ -2,12 +2,15 @@ package com.bhardwaj.portfolioapp
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.contact_item.view.*
@@ -35,6 +38,24 @@ class ContactAdapter(val contactList:ArrayList<ModelContact>): RecyclerView.Adap
         fun bindItems(contactme: ModelContact){
             itemView.ic_contact.setImageResource(contactme.icon)
             itemView.tv_contact.text = HtmlCompat.fromHtml(contactme.contact,Html.FROM_HTML_MODE_LEGACY)
+            itemView.tv_contact.setOnClickListener({
+                if(contactme.contact.contains("98")){
+                    Intent(Intent.ACTION_DIAL).also {
+                        it.setData(Uri.parse("tel:"+contactme.contact.trim()))
+                        itemView.context.startActivity(it)
+                    }
+                }else if (contactme.contact.contains("@")){
+                    Intent(Intent.ACTION_VIEW).also {
+                        it.setData(Uri.parse("mailto:"+contactme.contact.trim()))
+                        itemView.context.startActivity(it)
+                    }
+                }else{
+                    Intent(Intent.ACTION_VIEW).also {
+                        it.setData(Uri.parse("https://"+contactme.contact.trim()))
+                        itemView.context.startActivity(it)
+                    }
+                }
+            })
         }
     }
 
