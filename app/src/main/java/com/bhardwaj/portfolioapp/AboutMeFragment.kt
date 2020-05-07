5d8fab1d5161ffa2881fc3cfc4030f0bf00f908a
Ankit Bhardwaj <iamankitbhardwaj@gmail.com>
2020-05-07 11:16:38 +0530

    - Mostly final app - onClick implemented on Contact items - Added more info in about me tab Features to be added : - Reduce scrolling senstivity of viewpager
    Signed-off-by: Ankit Bhardwaj <iamankitbhardwaj@gmail.com>

--- app/src/main/java/com/bhardwaj/portfolioapp/AboutMeFragment.kt
+++ app/src/main/java/com/bhardwaj/portfolioapp/AboutMeFragment.kt
@@ -23,11 +23,20 @@ class AboutMeFragment : Fragment(R.layout.fragment_about_me) {
         aboutme.add(Model("EDUCATION","<b>&bull SRM University,Sonipat • Present</b><br>&ensp; Pursuing B.Tech. (C.S.E.) with IBM\n" +
                 "&ensp; Specialisation in Big Data Analytics<br><br><b>&bull Indus Public School • 2017</b><br>&ensp; Senior Secondary Education<br><br>" +
                 "<b>&bull Akash Sr. Sec. School • 2015</b><br>&ensp; Secondary Education"))
+        aboutme.add(Model("SKILLS","C&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;<big><big>&#9679&#9679&#9679&#9679&#9679</big></big><br>" +
+                "Java&ensp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;<big><big>&#9679&#9679&#9679&#9679&#9679</big></big><br>" +
+                "Python &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;<big><big>&#9679&#9679&#9679&#9679&#9675</big></big><br>" +
+                "Data Structures&ensp;&emsp;&ensp;&ensp;&nbsp;<big><big>&#9679&#9679&#9679&#9679&#9675</big></big><br>"+
+                "Algorithms&emsp;&emsp;&emsp;&emsp;&ensp;&ensp;<big><big>&#9679&#9679&#9679&#9679&#9675</big></big><br>" +
+                "Android Dev&emsp;&emsp;&emsp;&emsp;&nbsp;<big><big>&#9679&#9679&#9679&#9679&#9675</big></big><br>" +
+                "Kotlin&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;<big><big>&#9679&#9679&#9679&#9675&#9675</big></big><br>"))
+        aboutme.add(Model("LANGUAGES","English&emsp;&emsp;&emsp;&ensp;<big><big>&#9679&#9679&#9679&#9679&#9675</big></big><br>" +
+                "Hindi&ensp;&emsp;&emsp;&emsp;&emsp;<big><big>&#9679&#9679&#9679&#9679&#9679</big></big><br>" +
+                "German &emsp;&emsp;&emsp;<big><big>&#9679&#9679&#9675&#9675&#9675</big></big><br>"))
         aboutme.add(Model("EXPERIENCE","<b>&bull Fresher</b><br>&ensp; Not having any industrial<br>" +
                 "&ensp; experience yet but having<br>" +
                 "&ensp; enough technical experience to<br>" +
                 "&ensp; handle a S/W job."))
-
         val rvAdapter = Adapter(aboutme)
         rv_aboutme.adapter = rvAdapter
     }
